ec0f7e424c2bf90c2d5303a804c7c69f78e1656e
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2022-11-05 22:36:46 +0100

    card style not finished

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let currentPokemon;
 let currentPokemonImg;
+let id = 1;
+let currentId = 0;
+let loadLimit = 30;
 
 
 async function loadPokemonInfo() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
+    // for (let i = currentId; i < loadLimit.length; i++) {
+    //     let id =  i + 1;
+    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     let response = await fetch(url);
     currentPokemon = await response.json();
-    console.log('Loaded Pokemon', currentPokemon);
+    // console.log('Loaded Pokemon', currentPokemon);
     currentPokemonImg = currentPokemon['sprites']['other']['dream_world']['front_default'];
-    console.log('Loaded Pokemon IMG',currentPokemonImg)
-
     renderPokemonInfo();
+    // }
 }
 
 
