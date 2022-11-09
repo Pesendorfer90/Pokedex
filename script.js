8f2490c92605e1cca5384b92bfb2e500bf8e239a
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2022-11-09 14:54:41 +0100

    add for 30 times

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 let currentPokemon;
 let currentPokemonImg;
-let id = 1;
+let pokemonName;
+let id;
 let currentId = 0;
 let loadLimit = 30;
 
 
+
 async function loadPokemonInfo() {
-    // for (let i = currentId; i < loadLimit.length; i++) {
-    //     let id =  i + 1;
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    // console.log('Loaded Pokemon', currentPokemon);
-    currentPokemonImg = currentPokemon['sprites']['other']['dream_world']['front_default'];
-    renderPokemonInfo();
-    // }
+    for (let i = currentId; i < loadLimit; i++) {
+        id = i + 1;
+        let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        console.log('Loaded Pokemon', currentPokemon);
+        currentPokemonImg = currentPokemon['sprites']['other']['dream_world']['front_default'];
+        pokemonName = currentPokemon['name']
+
+        renderPokemonInfo();
+    }
 }
 
 
 function renderPokemonInfo() {
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImg').src = currentPokemonImg;
+    document.getElementById('cardContainer').innerHTML += `
+    <div class="card-container">
+        <div class="pokedex-card">
+
+            <div class="pokemon-ID">
+                <div class="ID-container">#${id}</div>
+            </div>
+
+            <div class="pokedex">
+                <div class="pokemon-info">
+                    <h2>${pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h2>
+                    <div class="pokemon-type">
+                        <span>grass</span>
+                        <span>poison</span>
+                    </div>
+                </div>
+                <div>
+                    <img class="pokemonImg" src="${currentPokemonImg}">
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
 }
